b30d2f7d20eaf93d4b7f87b0512f351e8731bae7
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-15 20:25:33 -0700

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116023607) do
+ActiveRecord::Schema.define(version: 20171116031149) do
 
   create_table "requests", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20171116023607) do
     t.integer "duration"
     t.integer "karmapoints"
     t.string "urgency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
