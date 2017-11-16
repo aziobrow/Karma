84fc01f37aa6aa178272112f4c59c35248f59f68
Emily Wise <wise.m.emily@gmail.com>
2017-11-15 20:03:48 -0700

    updated create method

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,6 +6,16 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.create(request_params)
+    redirect_to requests_path
   end
+
+
+  private
+
+  def request_params
+    params.require[:request].permit(:title, :description, :servicedate, :duration, :location, :karmapoints, :urgency)
+  end
+
+
 
 end
